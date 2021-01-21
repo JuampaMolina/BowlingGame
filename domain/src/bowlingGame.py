6fb05eee84dc902981c67d8315951c5415de4983
JuampaMolina <jruiz@cifpfbmoll.eu>
2021-01-21 04:42:03 +0100

    feat: bowling

--- domain/src/bowlingGame.py
+++ domain/src/bowlingGame.py
@@ -1,11 +1,67 @@
 class Bowling():
 
-def __init__(self, scorecard):
-    self.scorecard = list(score_card) # Convertimos el string score_card en una lista.
-    self.score = 0 # 
-    self.current_frame = 1
-    self.current_roll = 0
+    # Símbolos que representan tiradas especiales.
+    GUTTERBALL = '-'
+    SPARE = '/'
+    STRIKE = 'X'
 
-def game_score(self):
-    for roll in self.score_card:
-        if:
+    LAST_FRAME = 10
+    TOTAL_PINS = 10
+
+    def __init__(self, scorecard):
+        # Convertimos el string score_card en una lista.
+        self.scorecard = scorecard
+        self.score = 0
+        self.current_frame = 1
+        self.current_roll = 0
+        self.rolls_in_frame = 0
+        self.special = 0
+
+    def game_score(self):
+        for roll in self.scorecard:
+            if self.current_frame < self.LAST_FRAME:
+                if self.special > 0:
+                    self.special -= 1
+                    if roll == Bowling.GUTTERBALL:
+                        self.score += 0
+                    if roll == Bowling.STRIKE or roll == Bowling.SPARE:
+                        self.score += 10
+                    if roll.isdigit():
+                        self.score += int(roll)
+                if roll.isdigit():
+                    Bowling.rollNormal(self, roll)
+                if roll == Bowling.GUTTERBALL:
+                    Bowling.rollGutterball(self)
+                if roll == Bowling.SPARE:
+                    Bowling.rollSpare(self)
+                if roll == Bowling.STRIKE:
+                    Bowling.rollStrike(self)
+
+        return self.score
+
+    def rollDone(self):
+        self.rolls_in_frame += 1
+        if self.rolls_in_frame == 2:
+            self.current_frame += 1
+            self.rolls_in_frame = 0
+
+    def rollNormal(self, score):
+        Bowling.rollDone(self)
+        self.current_roll += 1
+        self.score += int(score)
+
+    def rollGutterball(self):
+        Bowling.rollDone(self)
+        self.score += 0
+
+    def rollSpare(self):
+        self.current_frame += 1
+        Bowling.rollDone(self)
+        self.score += 10
+        self.special += 1
+
+    def rollStrike(self):
+        self.current_frame += 1
+        Bowling.rollDone(self)
+        self.score += 10
+        self.special = 2
--- domain/test/test_bowlingGame.py
+++ domain/test/test_bowlingGame.py
@@ -1,5 +1,21 @@
-from domain.src.bowlingGame import Bowling
+from src.bowlingGame import Bowling
 import pytest
 
-def test_normal():
-    assert Bowling.normal('1')
+# https://www.bowlinggenius.com/
+
+
+def test_rollNormal():
+    assert Bowling('12345123451234512345').game_score() == 60
+
+
+def test_rollGutterball():
+    assert Bowling('-2345-2345-2345-2345').game_score() == 56
+
+
+def test_rollSpare():
+    assert Bowling('1/34512/451234512/45').game_score() == 93
+
+
+def test_rollStrike():
+    assert Bowling('12X5123X1234X2345').game_score() == 82
+    assert Bowling('12X5123XX34X2345').game_score() == 106
