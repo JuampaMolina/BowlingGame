3f9bbc95bfed609dd4bf1f35735f7d154c5e8ae7
JuampaMolina <jruiz@cifpfbmoll.eu>
2021-01-21 04:42:13 +0100

    feat: bowling

--- README.md
+++ README.md
@@ -30,3 +30,24 @@ Here are some things that the program will not do:
 - We will not check for valid rolls.
 - We will not check for correct number of rolls and frames.
 - We will not provide scores for intermediate frames.
+
+### Casos test
+
+The input is a scorecard from a finished bowling game, where “X” stands for a strike, “-” for no pins
+bowled, and “/” means a spare. Otherwise figures 1-9 indicate how many pins were knocked down in
+that throw.
+Sample games:
+
+12345123451234512345
+always hitting pins without getting spares or strikes, a total score of 60.
+
+XXXXXXXXXXXX
+a perfect game, 12 strikes, giving a score of 300.
+
+9-9-9-9-9-9-9-9-9-9-
+heartbreak - 9 pins down each round, giving a score of 90.
+
+5/5/5/5/5/5/5/5/5/5/5
+a spare every round, giving a score of 150.
+Como puedes imaginar, estos casos no van a ser suficientes. Intenta conseguir más ejemplos de
+partidas reales para chequear todos los posibles estados de las variables del programa.
--- domain/src/bowlingGame.py
+++ domain/src/bowlingGame.py
@@ -16,6 +16,7 @@ class Bowling():
         self.current_roll = 0
         self.rolls_in_frame = 0
         self.special = 0
+        self.strike_count = 0
 
     def game_score(self):
         for roll in self.scorecard:
@@ -24,44 +25,46 @@ class Bowling():
                     self.special -= 1
                     if roll == Bowling.GUTTERBALL:
                         self.score += 0
-                    if roll == Bowling.STRIKE or roll == Bowling.SPARE:
-                        self.score += 10
-                    if roll.isdigit():
+                    elif roll == Bowling.STRIKE or roll == Bowling.SPARE:
+                        self.score += 10 * self.strike_count
+                        self.strike_count -= 1
+                    elif roll.isdigit():
                         self.score += int(roll)
                 if roll.isdigit():
                     Bowling.rollNormal(self, roll)
-                if roll == Bowling.GUTTERBALL:
+                elif roll == Bowling.GUTTERBALL:
                     Bowling.rollGutterball(self)
-                if roll == Bowling.SPARE:
+                elif roll == Bowling.SPARE:
                     Bowling.rollSpare(self)
-                if roll == Bowling.STRIKE:
+                elif roll == Bowling.STRIKE:
                     Bowling.rollStrike(self)
 
         return self.score
 
-    def rollDone(self):
+    def updateRolls(self):
         self.rolls_in_frame += 1
         if self.rolls_in_frame == 2:
             self.current_frame += 1
             self.rolls_in_frame = 0
 
     def rollNormal(self, score):
-        Bowling.rollDone(self)
+        Bowling.updateRolls(self)
         self.current_roll += 1
         self.score += int(score)
 
     def rollGutterball(self):
-        Bowling.rollDone(self)
+        Bowling.updateRolls(self)
         self.score += 0
 
     def rollSpare(self):
         self.current_frame += 1
-        Bowling.rollDone(self)
+        Bowling.updateRolls(self)
         self.score += 10
         self.special += 1
 
     def rollStrike(self):
+        self.strike_count += 1
         self.current_frame += 1
-        Bowling.rollDone(self)
+        Bowling.updateRolls(self)
         self.score += 10
         self.special = 2
